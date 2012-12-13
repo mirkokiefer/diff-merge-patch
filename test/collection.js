aa97038d2deb443c5f82d983f1fdc6f9ea1f2567
Mirko Kiefer <mail@mirkokiefer.com>
2012-12-13 22:05:04 +0100

    add collection diff

---
+++ test/collection.js
@@ -0,0 +1,17 @@
+
+var assert = require('assert')
+var diff = require('../lib/collection')
+
+describe('collection diff', function() {
+  it('should find all added and deleted values', function() {
+    var before = [1,2,3,3,4]
+    var after = [4,1,2,5]
+
+    var expectedDiff = {
+      '-': [3, 3],
+      '+': [5]
+    }
+    var result = diff(before, after)
+    assert.deepEqual(result, expectedDiff)
+  })
+})
