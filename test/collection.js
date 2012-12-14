3cf85a7eaa0f9568e9d74a9d273638ff673c8c8b
Mirko Kiefer <mail@mirkokiefer.com>
2012-12-15 00:14:35 +0100

    better collection diff test

--- test/collection.js
+++ test/collection.js
@@ -4,11 +4,11 @@ var diff = require('../lib/index').collections
 
 describe('collection diff', function() {
   it('should find all added and deleted values', function() {
-    var before = [1,2,3,3,4]
-    var after = [4,1,2,5]
+    var before = [1, 2, 3, 3, 4, 8]
+    var after = [4, 1, 2, 5, 6, 6, 8]
 
     var expectedDiff = {
-      added: [5],
+      added: [5, 6, 6],
       deleted: [3, 3]
     }
     var result = diff(before, after)
