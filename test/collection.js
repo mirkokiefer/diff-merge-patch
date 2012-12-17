ef71abff86af22f6ced33cf8fc55d9dd8a64e42f
Mirko Kiefer <mail@mirkokiefer.com>
2012-12-17 16:03:13 +0100

    make collection diff consistend and output indexes for deletes

--- test/collection.js
+++ test/collection.js
@@ -8,21 +8,21 @@ describe('collection diff', function() {
     var after = [4, 1, 2, 5, 6, 6, 8]
 
     var expectedDiff = {
-      added: [5, 6, 6],
-      deleted: [3, 3]
+      insert: [5, 6, 6],
+      delete: [2, 3]
     }
     var result = diff(before, after)
     assert.deepEqual(result, expectedDiff)
 
     var before = [1,2,3,3,4]
     var after = [4,1,2,3,5]
-    var expected = { added: [ 5 ], deleted: [ 3 ] }
+    var expected = { insert: [ 5 ], delete: [ 3 ] }
     var result = diff(before, after)
     assert.deepEqual(result, expected)
 
     var before = [1,2,3,3,4]
     var after = [1,2,3]
-    var expected = { added: [], deleted: [ 3, 4 ] }
+    var expected = { insert: [], delete: [ 3, 4 ] }
     var result = diff(before, after)
     assert.deepEqual(result, expected)
   })
