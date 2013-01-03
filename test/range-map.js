d167a447fe38483cb7e5e514cb506dc62e973f6b
Mirko Kiefer <mail@mirkokiefer.com>
2013-01-03 18:59:39 +0100

    range merging with conflicts

--- test/range-map.js
+++ test/range-map.js
@@ -18,7 +18,31 @@ describe('range-map merging', function() {
       {range: [6, 7], value: 3}
     ]}
     var result = merge([map1, map2])
-    console.log(result.result)
+    assert.deepEqual(result, expected)
+  })
+  it ('should merge with conflicts', function() {
+    var map1 = [
+      {range: [1, 3], value: 1},
+      {range: [6, 8], value: 2}
+    ]
+    var map2 = [
+      {range: [2, 4], value: 5},
+      {range: [6, 7], value: 3}
+    ]
+    var expected = {
+      conflict: true,
+      result: [[
+          {range: [1, 3], value: 1},
+          {range: [4, 4], value: 5},
+          {range: [6, 8], value: 2},
+        ], [
+          {range: [1, 1], value: 1},
+          {range: [2, 4], value: 5},
+          {range: [6, 7], value: 3},
+          {range: [8, 8], value: 2}
+        ]]
+      }
+    var result = merge([map1, map2])
     assert.deepEqual(result, expected)
   })
 })
