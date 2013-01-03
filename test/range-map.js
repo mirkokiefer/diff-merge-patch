ae1b8331b234d4f4129dc95e21934e6af40893b1
Mirko Kiefer <mail@mirkokiefer.com>
2013-01-03 21:18:27 +0100

    n-way merging of ranges

--- test/range-map.js
+++ test/range-map.js
@@ -31,7 +31,8 @@ describe('range-map merging', function() {
     ]
     var expected = {
       conflict: true,
-      result: [[
+      result: [
+        [
           {range: [1, 3], value: 1},
           {range: [4, 4], value: 5},
           {range: [6, 8], value: 2},
@@ -40,9 +41,49 @@ describe('range-map merging', function() {
           {range: [2, 4], value: 5},
           {range: [6, 7], value: 3},
           {range: [8, 8], value: 2}
-        ]]
-      }
+        ]
+      ]
+    }
     var result = merge([map1, map2])
+    assert.deepEqual(result, expected)
+  })
+  it ('should merge with three range-maps', function() {
+    var map1 = [
+      {range: [1, 3], value: 1},
+      {range: [6, 8], value: 2}
+    ]
+    var map2 = [
+      {range: [2, 4], value: 5},
+      {range: [6, 7], value: 3}
+    ]
+    var map3 = [
+      {range: [2, 2], value: 8},
+      {range: [9, 10], value: 3}
+    ]
+    var expected = {
+      conflict: true,
+      result: [
+        [
+          {range: [1, 3], value: 1},
+          {range: [4, 4], value: 5},
+          {range: [6, 8], value: 2},
+          {range: [9, 10], value: 3},
+        ], [
+          {range: [1, 1], value: 1},
+          {range: [2, 4], value: 5},
+          {range: [6, 7], value: 3},
+          {range: [8, 8], value: 2},
+          {range: [9, 10], value: 3}
+        ], [
+          {range: [1, 1], value: 1},
+          {range: [2, 2], value: 8},
+          {range: [4, 4], value: 5},
+          {range: [8, 8], value: 2},
+          {range: [9, 10], value: 3}
+        ]
+      ]
+    }
+    var result = merge([map1, map2, map3])
     assert.deepEqual(result, expected)
   })
 })
