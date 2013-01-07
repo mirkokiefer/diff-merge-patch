c49fe4892df95813cbd65d4f3a4077006d737d2a
Mirko Kiefer <mail@mirkokiefer.com>
2013-01-07 19:59:29 +0100

    compute difference of collection diffs

--- test/collection.js
+++ test/collection.js
@@ -1,6 +1,7 @@
 
 var assert = require('assert')
 var diff = require('../lib/index').collections
+var Result = diff.Result
 
 describe('collection diff', function() {
   it('should find all added and deleted values', function() {
@@ -22,8 +23,24 @@ describe('collection diff', function() {
 
     var before = [1,2,3,3,4]
     var after = [1,2,3]
-    var expected = { insert: [], delete: [ 3, 4 ] }
+    var expected = { delete: [ 3, 4 ] }
     var result = diff(before, after)
+    assert.deepEqual(result, expected)
+  })
+  it('should compute the difference of two diffs', function() {
+    var diff1 = new Result({
+      insert: [5, 6, 6],
+      delete: [2, 3]
+    })
+    var diff2 = {
+      insert: [6, 7],
+      delete: [2]
+    }
+    var expected = {
+      insert: [5, 6],
+      delete: [3]
+    }
+    var result = diff1.difference(diff2)
     assert.deepEqual(result, expected)
   })
 })
