8f1fa96af471828939a5175e0a084104960cb457
Mirko Kiefer <mail@mirkokiefer.com>
2013-01-14 19:08:19 +0100

    tidy

--- test/array/diff.js
+++ test/array/diff.js
@@ -12,35 +12,5 @@ describe('array diff', function() {
       })
     })
   })
-  /*it('should do a non-set diff', function() {
-    var diff = require('../../lib/index').array.diff({unique:false})
-
-    var before = 'this is just a test sentece - let\'s see how it works.'
-    var after = 'Hi! This is just a test sentence - let\'s see if it works.'
-
-    var beforeWords = before.split(' ')
-    var afterWords = after.split(' ')
-    var expected = {
-      delete: [[0,1],[5,1],[9,1]],
-      insert: [[0,["Hi!","This"]],[5,["sentence"]],[9,["if"]]]
-    }
-
-    var result = diff(beforeWords, afterWords)
-    assert.deepEqual(result, expected)
-
-    var beforeChars = before.split('')
-    var afterChars = after.split('')
-    var expected = {
-      delete: [[0,1], [40,3]],
-      insert:[
-        [0,["H","i","!"," ","T"]],
-        [24,["n"]],
-        [42,["i","f"]]
-      ]
-    }
-
-    var result = diff(beforeChars, afterChars)
-    assert.deepEqual(result, expected)
-  })*/
 })
 
