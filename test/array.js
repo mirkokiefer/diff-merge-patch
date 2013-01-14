68181545b3ad57baa602fa6e45fcf5d411961d4e
Mirko Kiefer <mail@mirkokiefer.com>
2013-01-14 19:53:29 +0100

    refactor tests

--- test/array.js
+++ test/array.js
@@ -1,14 +1,7 @@
 
 var assert = require('assert')
-var array = require('../lib/index').array
-var diff = array.diff
-var merge = array.merge
-var Result = merge.Result
-var patch = array.patch
 
-var testData = require('./test-data/array')
-
-var testDiff = function(testData) { return function() {
+var testDiff = function(diff, testData) { return function() {
   testData.forEach(function(each, j) {
     each.after.forEach(function(eachAfter, i) {
       it('should find the position diff at ' + j + '.' + i, function() {
@@ -19,13 +12,14 @@ var testDiff = function(testData) { return function() {
   })
 }}
 
-var testMerge = function(testData) { return function() {
+var testMerge = function(merge, testData) { return function() {
   testData.forEach(function(each, i) {
     it('should merge the diffs at ' + i, function() {
       var result = merge([each.diffs[0], each.diffs[1]])
       assert.deepEqual(result, each.diffsMerged)
     })
   })
+  var Result = merge.Result
   testData.forEach(function(each, i) {
     if (each.diffsMergedResolved) {
       it('should test conflict resolution at ' + i, function() {
@@ -37,7 +31,7 @@ var testMerge = function(testData) { return function() {
   })
 }}
 
-var testPatch = function(testData) { return function() {
+var testPatch = function(patch, testData) { return function() {
   testData.forEach(function(each, i) {
     each.diffs.forEach(function(diff, j) {
       it('should patch an array using a diff at ' + i + '.' + j, function() {
@@ -48,8 +42,12 @@ var testPatch = function(testData) { return function() {
   })
 }}
 
+var test = function(module, testData) {
+  describe('diff', testDiff(module.diff, testData))
+  describe('merge', testMerge(module.merge, testData))
+  describe('patch', testPatch(module.patch, testData))
+}
+
 describe('array', function() {
-  describe('diff', testDiff(testData))
-  describe('merge', testMerge(testData))
-  describe('patch', testPatch(testData))
+  test(require('../lib/index').array, require('./test-data/array'))
 })
