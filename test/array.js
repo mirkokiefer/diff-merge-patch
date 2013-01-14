8b7560533b8448ffcb7452af654e704c873b7a0d
Mirko Kiefer <mail@mirkokiefer.com>
2013-01-14 19:34:13 +0100

    more test refactoring

--- test/array.js
+++ test/array.js
@@ -8,46 +8,48 @@ var patch = array.patch
 
 var testData = require('./test-data/array')
 
+var testDiff = function(testData) { return function() {
+  testData.forEach(function(each, j) {
+    each.after.forEach(function(eachAfter, i) {
+      it('should find the position diff at ' + j + '.' + i, function() {
+        var result = diff(each.before, eachAfter)
+        assert.deepEqual(result, each.diffs[i])
+      })
+    })
+  })
+}}
+
+var testMerge = function(testData) { return function() {
+  testData.forEach(function(each, i) {
+    it('should merge the diffs at ' + i, function() {
+      var result = merge([each.diffs[0], each.diffs[1]])
+      assert.deepEqual(result, each.diffsMerged)
+    })
+  })
+  testData.forEach(function(each, i) {
+    if (each.diffsMergedResolved) {
+      it('should test conflict resolution at ' + i, function() {
+        var diff = new Result(each.diffsMerged.diff, each.diffsMerged.conflict)
+        var resolved = diff.resolveConflicts()
+        assert.deepEqual(resolved, each.diffsMergedResolved)
+      })
+    }
+  })
+}}
+
+var testPatch = function(testData) { return function() {
+  testData.forEach(function(each, i) {
+    each.diffs.forEach(function(diff, j) {
+      it('should patch an array using a diff at ' + i + '.' + j, function() {
+        var result = patch(each.before, diff)
+        assert.deepEqual(result, each.after[j])
+      })
+    })
+  })
+}}
+
 describe('array', function() {
-
-  describe('diff', function() {
-    testData.forEach(function(each, j) {
-      each.after.forEach(function(eachAfter, i) {
-        it('should find the position diff at ' + j + '.' + i, function() {
-          var result = diff(each.before, eachAfter)
-          assert.deepEqual(result, each.diffs[i])
-        })
-      })
-    })
-  })
-
-  describe('merge', function() {
-    testData.forEach(function(each, i) {
-      it('should merge the diffs at ' + i, function() {
-        var result = merge([each.diffs[0], each.diffs[1]])
-        assert.deepEqual(result, each.diffsMerged)
-      })
-    })
-    testData.forEach(function(each, i) {
-      if (each.diffsMergedResolved) {
-        it('should test conflict resolution at ' + i, function() {
-          var diff = new Result(each.diffsMerged.diff, each.diffsMerged.conflict)
-          var resolved = diff.resolveConflicts()
-          assert.deepEqual(resolved, each.diffsMergedResolved)
-        })
-      }
-    })
-  })
-
-  describe('patch', function() {
-    testData.forEach(function(each, i) {
-      each.diffs.forEach(function(diff, j) {
-        it('should patch an array using a diff at ' + i + '.' + j, function() {
-          var result = patch(each.before, diff)
-          assert.deepEqual(result, each.after[j])
-        })
-      })
-    })
-  })
-
+  describe('diff', testDiff(testData))
+  describe('merge', testMerge(testData))
+  describe('patch', testPatch(testData))
 })
