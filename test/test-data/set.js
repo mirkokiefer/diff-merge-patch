9e62e128e26e24b1f6052ac30358b784d75ffb3d
Mirko Kiefer <mail@mirkokiefer.com>
2015-05-01 22:17:32 +0200

    reproduce set merge issue #3

--- test/test-data/set.js
+++ test/test-data/set.js
@@ -16,5 +16,23 @@ module.exports = [
         {insert: 5, source: [0, 1]}, {insert: 6, source: [0]}, {insert: 7, source: [1]}
       ]
     }
+  },
+  {
+    before: [1, 2, 3],
+    after: [
+      [1, 2, 5],
+      [1, 2, 4, 5]
+    ],
+    diffs: [
+      {diff:[{delete: 3}, {insert: 5}]},
+      {diff:[{delete: 3}, {insert: 4}, {insert: 5}]}
+    ],
+    diffsMerged: {
+      diff: [
+        {source:[0,1], delete: 3},
+        {source:[1], insert: 4},
+        {source:[1], insert: 5}
+      ]
+    }
   }
 ]
